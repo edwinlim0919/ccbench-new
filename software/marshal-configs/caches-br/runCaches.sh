bb3ec2aa00dc9d88834fae3e8e23745b909564ba
Cloud User <centos@ip-192-168-5-228.ec2.internal>
2022-06-03 04:58:58 +0000

    both strided and caches workload are passing software simulation tests

--- software/marshal-configs/caches-br/runCaches.sh
+++ software/marshal-configs/caches-br/runCaches.sh
@@ -1,19 +1,18 @@
-# set -x
-#
 # TODO: check if LLC size is correct
 # LLC (combined) size = 2^27 bytes = 134217728 bytes = 33554432 ints
 # Sweeping across size to obtain LLC curve => 67108864 ints
 
 # increasing array size by 10% each time (minimum AppSize is 32)
 SIZE=32
-PREV_RUN_SIZE=0
+PREV_SIZE=0
 DIV_AMOUNT=10
 MAX_SIZE=67108864
+NUM_ITERATIONS=100
 RUN_COUNT=10
 
-cd root/caches
+cd /root/caches
 
-# delete results file if it already exists
+# clear results file if it already exists
 if [ -f "../run_result.csv" ]
 then
     rm ../run_result.csv
@@ -25,7 +24,7 @@ while [ $SIZE -le $MAX_SIZE ]
 do
     for (( i=0; i<$RUN_COUNT; ++i ))
     do
-        ./caches $SIZE $SIZE 0 >> ../run_result.csv
+        ./caches $SIZE $NUM_ITERATIONS 0 >> ../run_result.csv
     done
 
     NEW_SIZE=$((SIZE+(SIZE/DIV_AMOUNT)))
@@ -40,34 +39,4 @@ do
     fi
 done
 
-#while [ $SIZE -le $MAX_SIZE ]
-#do
-#    if [ $SIZE -eq 32]
-#    then
-#        ./caches $SIZE $SIZE 0 > ../run_result.csv
-#        for (( i=1; i<($RUN_COUNT-1); ++i ))
-#        do
-#            ./caches $SIZE $SIZE 0 >> ../run_result.csv
-#        done
-#        PREV_RUN_SIZE=$SIZE
-#        SIZE=$((SIZE+32))
-#    else
-#        # if DIFF < 32, just increment AppSize by 32
-#        DIFF=$((SIZE-PREV_RUN_SIZE))
-#        if [ $DIFF -lt 32 ]
-#        then
-#            SIZE=$((PREV_RUN_SIZE+32))
-#        else
-#            for (( i=0; i<($RUN_COUNT-1); ++i ))
-#            do
-#                ./caches $SIZE $SIZE 0 >> ../run_result.csv
-#            done
-#            ./caches $SIZE $SIZE 0 >> ../run_result.csv
-#            PREV_RUN_SIZE=$SIZE
-#            SIZE=$((SIZE+(SIZE/DIV_AMOUNT)))
-#        fi
-#    fi
-#done
-
 poweroff
-
--- software/marshal-configs/strided-br/runStrided.sh
+++ software/marshal-configs/strided-br/runStrided.sh
@@ -1,27 +1,51 @@
-# set -x
-#
 # TODO: check if LLC size is correct
 # LLC (combined) size = 2^27 bytes = 134217728 bytes = 33554432 ints
 # Sweeping across size to obtain LLC curve => 67108864 ints
 
-# increasing array size by 5% each time (minimum AppSize is 32)
+# increasing array size by 10% each time (minimum AppSize is 32)
 SIZE=32
 PREV_SIZE=0
-DIV_AMOUNT=20
-MAX_SIZE=67108864
+DIV_AMOUNT=10
+#MAX_SIZE=67108864
+MAX_SIZE=1024
+NUM_ITERATIONS=100
 RUN_COUNT=10
+
+cd /root/strided
+
+# clear results file if it already exists
+if [ -f "../run_result.csv" ]
+then
+    rm ../run_result.csv
+    touch ../run_result.csv
+fi
 
 # sweep exponentially across array sizes
 while [ $SIZE -le $MAX_SIZE ]
 do
-    # RUN STRIDED HERE
     for (( i=0; i<$RUN_COUNT; ++i ))
     do
-        ./strided 1 $SIZE 1 $SIZE
-        ./strided 1 $SIZE 4 $SIZE
-        ./strided 1 $SIZE 16 $SIZE
-        ./strided 1 $SIZE 64 $SIZE
-        ./strided 1 $SIZE 256 $SIZE
+        ./strided 1 $SIZE 1   $NUM_ITERATIONS >> ../run_result.csv
+    done
+
+    for (( i=0; i<$RUN_COUNT; ++i ))
+    do
+	./strided 1 $SIZE 4   $NUM_ITERATIONS >> ../run_result.csv
+    done
+
+    for (( i=0; i<$RUN_COUNT; ++i ))
+    do
+        ./strided 1 $SIZE 16  $NUM_ITERATIONS >> ../run_result.csv
+    done
+
+    for (( i=0; i<$RUN_COUNT; ++i ))
+    do
+        ./strided 1 $SIZE 64  $NUM_ITERATIONS >> ../run_result.csv
+    done
+
+    for (( i=0; i<$RUN_COUNT; ++i ))
+    do
+        ./strided 1 $SIZE 256 $NUM_ITERATIONS >> ../run_result.csv
     done
 
     NEW_SIZE=$((SIZE+(SIZE/DIV_AMOUNT)))
@@ -32,6 +56,8 @@ do
     then
         SIZE=$((PREV_SIZE+32))
     else
-        SIZE=$((SIZE+(SIZE/DIV_AMOUNT)))
+        SIZE=$NEW_SIZE
     fi
 done
+
+poweroff
